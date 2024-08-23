67fcf552380d048874a8cd831703ea52c84ed040
lryan8959 <lryan8959@gmail.com>
2024-08-24 02:59:33 +0500

    redisclient fix

--- src/cache.redis/cache.redis.module.ts
+++ src/cache.redis/cache.redis.module.ts
@@ -1,48 +1,29 @@
 import { Global, Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import type { RedisClientOptions } from 'redis';
-import { /*CacheInterceptor */ CacheModule } from '@nestjs/cache-manager';
+import { CacheModule, CacheModuleOptions } from '@nestjs/cache-manager';
 import { redisStore } from 'cache-manager-redis-store';
-// import { APP_INTERCEPTOR } from '@nestjs/core';
-
-
-const nodeEnv = process.env.NODE_ENV; // 'development'
-const port = process.env.PORT; // '3120'
-const mongodbUri = process.env.MONGODB_URI; // MongoDB connection URI
-const elasticsearchNode = process.env.ELASTICSEARCH_NODE; // '' (empty string)
-const elasticsearchUsername = process.env.ELASTICSEARCH_USERNAME; // 'elastic'
-const elasticsearchPassword = process.env.ELASTICSEARCH_PASSWORD; // 'g51i3KXTsBmoZ4ay'
-const redisHost = process.env.REDIS_HOST; // 'localhost'
-const redisPort = process.env.REDIS_PORT; // '6379'
-const redisPassword = process.env.REDIS_PASSWORD; // 'g51i3KTsREDISBmoZ4ay'
-
 
 @Global()
 @Module({
   imports: [
-    CacheModule.registerAsync<RedisClientOptions>({
+    ConfigModule.forRoot({
+      isGlobal: true, // Makes the configuration available globally
+    }),
+    CacheModule.registerAsync<CacheModuleOptions>({
       imports: [ConfigModule],
-      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-      // @ts-ignore
-      useFactory: async (configService: ConfigService) => ({
+      useFactory: async (configService: ConfigService): Promise<CacheModuleOptions> => ({
         store: await redisStore({
           socket: {
             host: process.env.REDIS_HOST,
             port: parseInt(process.env.REDIS_PORT, 10),
           },
-          password: configService.get<string>('REDIS_PASSWORD'),
-          ttl: 24 * 60 * 60 * 1000, // Set TTL to 1 day (24 hours) in milliseconds
-        }),
+          password: process.env.REDIS_PASSWORD,
+          ttl: 24 * 60 * 60, // Set TTL to 1 day (24 hours) in seconds
+        }) as any, // Cast to any to bypass type issues
       }),
       inject: [ConfigService],
     }),
   ],
   exports: [CacheModule],
-  providers: [
-    // {
-    //   provide: APP_INTERCEPTOR, //For Auto-caching
-    //   useClass: CacheInterceptor, //For Auto-caching
-    // },
-  ],
 })
 export class CacheRedisModule {}
